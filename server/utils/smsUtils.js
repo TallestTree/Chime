1a7d67c20183f646a3f86e503ba433d11edef41a
Joseph Pei <pei_joseph@yahoo.com>
2015-05-01 11:15:49 -0700

    [feature] Adds a working smsUtils and its associated tests  - Adds an example config file

--- server/utils/smsUtils.js
+++ server/utils/smsUtils.js
@@ -1,3 +1,31 @@
-module.exports = function(smsOptions, cb) {
+var voicejs = require('voice.js');
+var config = require('../config/config');
+var _ = require('underscore');
 
+// Create reusable client object
+var client = new voicejs.Client({
+  email: config.gmail.email,
+  password: config.gmail.password
+});
+
+module.exports = function(smsOptions, cb) {
+  // Use voice.js for Google Voice
+  smsOptions = smsOptions || {};
+  _.defaults(smsOptions, {
+    text: 'You have an anonymous visitor. - Tallest Tree App',
+  });
+
+  cb = cb || function(err, response, data) {
+    if(err || !response.body.ok) {
+      console.error(err || 'Error sending text: ' + response.body.data.code);
+    } else {
+      console.log('Text sent: ', response);
+    }
+  };
+
+  if (!smsOptions.to) {
+    cb('Error: No destination address for text');
+  } else {
+    client.altsms(smsOptions, cb);
+  }
 };
