d02dbdacb74845b703ff4510d21a223d2764bb6b
LanceFulton <lsfaccounts@gmail.com>
2015-05-15 12:09:02 -0700

    [bugfix] Corrects conflicts with package.json and missing config.example.js file

---
+++ server/config/config.example.js
@@ -0,0 +1,27 @@
+module.exports = {
+  gmail: {
+    email: 'YourEmailHere@gmail.com',
+    username: 'YourNameHere',
+    password: 'YourPasswordHere'
+  },
+  proddb: {
+    config: {
+      user: 'YourNameHere',
+      database: 'YourDbHere',
+      password: 'YourPasswordHere',
+      port: 5432,
+      host: 'www.yourhosthere.com',
+      ssl: true
+    }
+  },
+  testdb: {
+    config: {
+      user: 'YourNameHere',
+      database: 'YourDbHere',
+      password: 'YourPasswordHere',
+      port: 5432,
+      host: 'www.yourhosthere.com',
+      ssl: true
+    }
+  }
+};
